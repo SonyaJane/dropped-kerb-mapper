4e627d2783bcbe88d5e6dbb7502eb807b1d7d03d
SonyaJane <sonya.ridden@gmail.com>
2025-03-19 15:20:26 +0000

    Add Summernote for admin

--- mapper/admin.py
+++ mapper/admin.py
@@ -1,15 +1,17 @@
 from django.contrib import admin
 from .models import Report, Photo
+from django_summernote.admin import SummernoteModelAdmin
+
 # Register your models here.
 
-# class ReportAdmin(admin.ModelAdmin):
-#     list_display = ('classification', 'user', 'created_at')
+# Use a decorator to register a class, compared to just registering the standard model
+@admin.register(Report)
+class ReportAdmin(SummernoteModelAdmin):
+    list_display = ('id', 'classification', 'reasons', 'user')
+    search_fields = ['classification', 'reasons', 'user']
+    list_filter = ('classification', 'reasons', 'user')
+    #prepopulated_fields = {'slug': ('classification',)}
+    summernote_fields = ('comments',)
     
-#     class Media:
-#         js = ('js/report_admin.js',)
-
-#admin.site.register(Report, ReportAdmin)
-
-admin.site.register(Report)
 admin.site.register(Photo)
 
