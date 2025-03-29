7a04d582cc1091d6cda683ae9239e31ffcdb0865
SonyaJane <sonya.ridden@gmail.com>
2025-03-29 22:40:04 +0000

    Add edit report function

--- mapper/urls.py
+++ mapper/urls.py
@@ -1,9 +1,11 @@
 from . import views # import the views file 
 from django.urls import path
+from .views import ReportEditView
 
 urlpatterns = [
     path('', views.home, name='home'),  # Home page
     path('create-report/', views.create_report, name='create-report'),
     path('reports', views.ReportList.as_view(), name='reports-list'),
-    path('reports/<int:pk>/', views.report_detail, name='report_detail'),
+    path('reports/<int:pk>/', views.report_detail, name='report-detail'),
+    path('reports/<int:pk>/edit/', ReportEditView.as_view(), name='report-edit'),
 ]
--- mapper/views.py
+++ mapper/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.views import generic
 from .forms import ReportForm
 from .models import Report
+from django.http import HttpResponseRedirect
+from django.contrib import messages
+from django.urls import reverse_lazy
+from django.views.generic.edit import UpdateView
 
 def home(request):
     """
@@ -34,3 +38,13 @@ def create_report(request):
     else:
         form = ReportForm()
     return render(request, 'mapper/create_report.html', {'form': form})
+
+
+class ReportEditView(UpdateView):
+    model = Report
+    form_class = ReportForm
+    template_name = 'mapper/report_edit.html'
+
+    def get_success_url(self):
+        # Redirect to the report detail page after editing
+        return reverse_lazy('report-detail', kwargs={'pk': self.object.pk})
