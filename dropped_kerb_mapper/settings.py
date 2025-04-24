3cd4b26e4c2ba29690b94a028ec35a2b1f9af698
SonyaJane <sonya.ridden@gmail.com>
2025-04-24 22:05:19 +0100

    Changed allowed hosts

--- dropped_kerb_mapper/settings.py
+++ dropped_kerb_mapper/settings.py
@@ -21,7 +21,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.azurewebsites.net','.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['www.droppedkerbmapper.com','.herokuapp.com','127.0.0.1']
 
 # Application definition
 
