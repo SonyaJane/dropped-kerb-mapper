62e7bec42c09ede75c0162ade85cca1d9b20b0fd
SonyaJane <sonya.ridden@gmail.com>
2025-05-21 21:40:23 +0100

    CSS: only apply hover styles on desktop and laptop

--- dropped_kerb_mapper/settings.py
+++ dropped_kerb_mapper/settings.py
@@ -14,7 +14,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['www.droppedkerbmapper.com','.herokuapp.com','127.0.0.1']
 
