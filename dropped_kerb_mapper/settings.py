6efb9a2c8e037167d88e05547754279522cb0de0
SonyaJane <sonya.ridden@gmail.com>
2026-01-26 14:22:52 +0000

    Added DEFAULT_FROM_EMAIL

--- dropped_kerb_mapper/settings.py
+++ dropped_kerb_mapper/settings.py
@@ -126,10 +126,10 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")  # Gmail login (sonya.ridden@gmail.com)
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")  # Use App Password
 
-DEFAULT_FROM_EMAIL = os.environ.get("EMAIL_HOST_USER")
+DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL")
 ACCOUNT_DEFAULT_FROM_EMAIL = DEFAULT_FROM_EMAIL
 SERVER_EMAIL = DEFAULT_FROM_EMAIL
 
