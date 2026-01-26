c6c04706cc54f075b6ba222b6ac82133255d5012
SonyaJane <sonya.ridden@gmail.com>
2026-01-26 14:35:37 +0000

    Remove debug

--- dropped_kerb_mapper/settings.py
+++ dropped_kerb_mapper/settings.py
@@ -16,7 +16,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 GOOGLE_MAPS_API_KEY = os.environ.get("GOOGLE_MAPS_API_KEY")
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['www.droppedkerbmapper.com','.herokuapp.com','127.0.0.1','.northeurope.azurecontainerapps.io']
 
